0bec8b3e9a83da0c946ca8a38ca7dce7caed7fdc
joeshu <sccdsix@163.com>
2021-02-02 15:47:35 +0800

    fix ignore file upload misc

---
+++ Assets/TwoWheelRobot/Bike.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bike : MonoBehaviour
+{
+    public WheelCollider LWC, RWC;
+    public Rigidbody RBD;
+    public float WheelPower = 50;
+    public Transform LW, RW;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        RBD.centerOfMass = new Vector3(0, -5, 0);
+    }
+
+    private void FixedUpdate()
+    {
+        //LW.rotation = LWC.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
